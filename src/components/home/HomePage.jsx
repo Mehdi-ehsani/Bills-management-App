9848bd98f4c6bfd5dea1d04f8b89f368a23f50eb
Mahdi Ehsani <metti.work@gmail.com>
2024-05-27 23:58:30 +0330

    STYLE TO BILLS

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -22,30 +22,38 @@ const HomePage = () => {
 	const date = useSelector(dateSelector);
 	const tracking = useSelector(trackingSelector);
 
-	const [data , setData] = useState([])
+	const [data, setData] = useState([]);
+	const [isDelete, setIsDelete] = useState(false);
+
+	let localData = JSON.parse(localStorage.getItem("data")) || [];
 
 	//----------in start----------//
 
 	useEffect(() => {
 		setData(JSON.parse(localStorage.getItem("data")) || []);
 	}, []);
-	const addToLocalStorage = (title , description , date , cost , track) => {
-		// setData(JSON.parse(localStorage.getItem("data")) || []);
-		
-		data.push({title , description , date , cost , track });
+    
 
-		localStorage.setItem("data" , JSON.stringify(data))
+	const addToLocalStorage = (title, description, date, cost, track) => {
+		// setData(JSON.parse(localStorage.getItem("data")) || []);
+
+		data.push({ title, description, date, cost, track });
+
+		localStorage.setItem("data", JSON.stringify(data));
 
 		dispatch(ADD_COST(""));
 		dispatch(ADD_DESCRIPTION(""));
 		dispatch(ADD_TRACKING(""));
 		dispatch(ADD_TITLE(""));
 		dispatch(ADD_DATE(""));
-	}
+	};
 	//----------in data change----------//
 	const deleteBill = (title) => {
-		Array.prototype.remove = function() {
-			var what, a = arguments, L = a.length, ax;
+		Array.prototype.remove = function () {
+			var what,
+				a = arguments,
+				L = a.length,
+				ax;
 			while (L && this.length) {
 				what = a[--L];
 				while ((ax = this.indexOf(what)) !== -1) {
@@ -54,67 +62,96 @@ const HomePage = () => {
 			}
 			return this;
 		};
-		data.map(bill => {
-			bill.title == title ? data.remove(bill) : null
-		})
-		setData(data)
-		localStorage.setItem("data" , JSON.stringify(data))
+		data.map((bill) => {
 		
+		bill.title == title ? data.remove(bill)  : null;
+		});
+		localStorage.setItem("data", JSON.stringify(data));
+		setData(JSON.parse(localStorage.getItem("data")));
+       
+
+		// window.location.reload()
 	};
 
 	return (
 		<div>
 			<h1>سلام</h1>
-			<form >
-			<input
-				className="border-2 border-sky-500"
-				type="text"
-				value={title}
-				placeholder="نام قبض"
-				onChange={(e) => dispatch(ADD_TITLE(e.target.value))}
-			/>
-			<input
-				className="border-2 border-sky-500"
-				type="text"
-				value={date}
-				onChange={(e) => dispatch(ADD_DATE(e.target.value))}
-			/>
-			<input
-				className="border-2 border-sky-500"
-				type="text"
-				value={description}
-				placeholder="یادداشت"
-				onChange={(e) => dispatch(ADD_DESCRIPTION(e.target.value))}
-			/>
-			<input
-				className="border-2 border-sky-500"
-				type="text"
-				value={tracking}
-				placeholder="شماره پیگیری"
-				onChange={(e) => dispatch(ADD_TRACKING(e.target.value))}
-			/>
-			<input
-				className="border-2 border-sky-500"
-				type="text"
-				value={cost}
-				placeholder="مبلغ"
-				onChange={(e) => dispatch(ADD_COST(e.target.value))}
-			/>
-			<button onClick={() => addToLocalStorage(title , description , date , cost , tracking)}>add</button>
+			<form onClick={e => e.preventDefault()} >
+				<input
+					className="border-2 border-sky-500"
+					type="text"
+					value={title}
+					maxLength={20}
+					placeholder="نام قبض"
+					onChange={(e) => dispatch(ADD_TITLE(e.target.value))}
+				/>
+				<input
+					className="border-2 border-sky-500"
+					type="text"
+					value={date}
+					onChange={(e) => dispatch(ADD_DATE(e.target.value))}
+				/>
+				<input
+					className="border-2 border-sky-500"
+					type="text"
+					value={description}
+					placeholder="یادداشت"
+					onChange={(e) => dispatch(ADD_DESCRIPTION(e.target.value))}
+				/>
+				<input
+					className="border-2 border-sky-500"
+					type="text"
+					value={tracking}
+					placeholder="شماره پیگیری"
+					onChange={(e) => dispatch(ADD_TRACKING(e.target.value))}
+				/>
+				<input
+					className="border-2 border-sky-500"
+					type="text"
+					value={cost}
+					placeholder="مبلغ"
+					onChange={(e) => dispatch(ADD_COST(e.target.value))}
+				/>
+				<button
+					onClick={() =>
+						addToLocalStorage(title, description, date, cost, tracking)
+					}
+				>
+					add
+				</button>
 			</form>
 
 			<div className="grid grid-cols-3 gap-4 my-8">
-				{data.map(bill => (
-					<div className="bg-gray-200 border-r-2 border-yellow-400" key={bill.title}>
-                       <p className="">{bill.title}</p>
-                       <p>{bill.description}</p>
-                       <div>
-                       <p>{bill.track}</p>
-                       <p>{bill.date}</p>
-					   </div>
-					   <hr />
-					   <p>{bill.cost}</p>
-					   <button onClick={() => deleteBill(bill.title)} >delete</button>
+				{data.map((bill) => (
+					<div
+						className={isDelete ? "hidden" :"block bg-gray-200 border-r-2 border-yellow-400 p-3 rounded-xl shadow-md"}
+						key={bill.title}
+					>
+						<p className="text-2xl font-bold mb-2">{bill.title}</p>
+						<p>
+							<span className="text-lg text-yellow-600 font-semibold">
+								توضیحات:
+							</span>
+							{bill.description ? bill.description : "مشخص نشده"}
+						</p>
+						<div>
+							<p className="text-lg mt-1">
+								<span className="text-lg text-yellow-600 font-semibold">
+									کد پیگیری:
+								</span>
+								{bill.track ? bill.track : "مشخص نشده"}
+							</p>
+							<p className="text-lg mt-1">
+								<span className="text-lg text-yellow-600 font-semibold">
+									تاریخ:
+								</span>
+								{bill.date ? bill.date : "مشخص نشده"}
+							</p>
+						</div>
+						<div className="flex gap-2 mt-3">
+							<p className="w-2/3 h-8 bg-yellow-500 text-xl rounded-md font-semibold text-center leading-8 shadow-md"> {bill.cost} <span>تومان</span> </p>
+							<button className="w-1/3 h-8 text-xl leading-8 font-semibold border-2 cursor-pointer text-red-500 rounded-md border-red-500 hover:bg-red-500 hover:text-white transition-colors" onClick={() => deleteBill(bill.title)}>حذف</button>
+						</div>
 					</div>
 				))}
 			</div>
