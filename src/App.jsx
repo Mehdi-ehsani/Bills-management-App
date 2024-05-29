7449f9a70c06ae1e205cb389bfb395aa292442f2
Mahdi Ehsani <metti.work@gmail.com>
2024-05-29 14:18:58 +0330

    add routs to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import HomePage from "./components/home/HomePage"
+import About from "./components/pages/about/About"
+import Contact from "./components/pages/contact/Contact"
+import Bills from "./components/pages/bills/Bills"
+import { Route , Routes  } from "react-router-dom"
 
 function App() {
 
 
   return (
     <>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/bills" element={<Bills/>} />
+        <Route path="*" element={<HomePage/>} />
+      </Routes>
     </>
   )
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav className='flex my-8 h-20 items-center justify-between p-4 bg-gradient-to-tl from-yellow-400 to-yellow-500 rounded-2xl shadow-lg'>
        <h1 className='font-bold text-3xl'>قبضینو</h1>
-        <ul className='flex items-center gap-8 text-xl font-medium'>    
-          <li><Link>خانه</Link></li>
-          <li> <Link>قبض ها</Link></li>
-          <li> <Link>درباره ما</Link></li>
-          <li><Link>تماس با ما</Link></li>
+        <ul className='flex items-center gap-8 text-xl '>    
+          <li><Link to="/">خانه</Link></li>
+          <li> <Link to="/bills">قبض ها</Link></li>
+          <li> <Link to="/about">درباره ما</Link></li>
+          <li><Link to="/contact">تماس با ما</Link></li>
         </ul>
         <button className='rounded-full w-10 h-10 bg-white leading-10 text-center shadow-md' >☀</button>
     </nav>
